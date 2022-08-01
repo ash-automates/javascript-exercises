05ba45bf89e9beb03b60f2585db699346f5d3604
shachem <sekki.hachem@outlook.com>
2022-08-01 07:08:36 +0100

    Solve exercise 04 in the repository

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (targetArray) {
+    ItemsToRemove = []
+    for (let index = 1; index < arguments.length; index++) {
+        ItemsToRemove.push(arguments[index]);
+    }
+    function toBeKept(element) {
+        if (ItemsToRemove.indexOf(element) !== -1) {
+            return false;
+        }
+        return true;
+    }
+    return targetArray.filter(toBeKept);
 };
-
+removeFromArray([1, 2, 3], 1, 2, 3);
 // Do not edit below this line
 module.exports = removeFromArray;
