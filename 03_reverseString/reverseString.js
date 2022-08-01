ff8596b471caefeef840a92f936a1b5d68a52375
shachem <sekki.hachem@outlook.com>
2022-08-01 05:16:18 +0100

    Solve exercise 03 in the repository

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (sentence) {
+    let counter = sentence.length - 1;
+    let reversedSentence = ''
+    while (counter >= 0) {
+        reversedSentence += sentence[counter];
+        counter -= 1;
+    }
+    return reversedSentence;
 };
 
 // Do not edit below this line
