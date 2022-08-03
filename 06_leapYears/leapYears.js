bdc2b2d0c4ab7c24db6f8b4f3c7ba6fb99610220
shachem <sekki.hachem@outlook.com>
2022-08-03 13:40:10 +0100

    Solve exercise 06 in the repository

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function (yearToTest) {
+    if (yearToTest % 4 === 0 && yearToTest % 400 === 0 ||
+        yearToTest % 4 === 0 && yearToTest % 100 !== 0) {
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
