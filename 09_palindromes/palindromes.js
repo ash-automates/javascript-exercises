d5a9967990235a848b42776834ebaf38597295d5
shachem <sekki.hachem@outlook.com>
2022-08-07 18:43:15 +0100

    Solve exercise 09 within the repository

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const palindromes = function (rawSentence) {
+    const PUNCTUATION = ' !"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
+    const rawSentenceToArray = rawSentence.split('');
+    const cleanSentence = rawSentenceToArray.filter(function (char) {
+        return PUNCTUATION.indexOf(char) === -1;
+    }).join('');
+
+    let reversedSentence = '';
+    const iterate = cleanSentence.length - 1;
+    while (iterate >= 0) {
+        reversedSentence += cleanSentence[iterate];
+        iterate--;
+    }
+
+    if (reversedSentence.toLowerCase() === cleanSentence.toLowerCase()) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
