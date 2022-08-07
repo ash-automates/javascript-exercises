15779c6bd520315492000227c78a4b88bda6eada
shachem <sekki.hachem@outlook.com>
2022-08-07 19:03:20 +0100

    Solve exercise 11 within the repository

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (bookObjs) {
+    const bookTitles = [];
+    let iterate = 0;
+    while (iterate < bookObjs.length) {
+        bookTitles.push(bookObjs[iterate].title);
+        iterate++;
+    }
+    return bookTitles;
 };
 
 // Do not edit below this line
