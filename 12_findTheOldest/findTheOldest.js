5a6d2c4fb36143c813d290ddcec716fd299703c1
shachem <sekki.hachem@outlook.com>
2022-08-07 20:30:41 +0100

    Solve exercise 12 within the repository

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    function getAge(birthYear, deathYear) {
+        if (!deathYear) {
+            deathYear = (new Date()).getFullYear();
+        }
+        return deathYear - birthYear;
+    }
 
+    const sortedByAge = people.sort(function (current, next) {
+        if (getAge(current.yearOfBirth, current.yearOfDeath) > getAge(next.yearOfBirth, next.yearOfDeath)) {
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+
+    return sortedByAge[0];
 };
 
 // Do not edit below this line
